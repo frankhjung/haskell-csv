8d5d54de24b0d3e994c7092a3b00ec1cfe8fa649
Frank Jung <frankhjung@linux.com>
2018-08-06 11:51:07 +1000

    added string and qualified imports

--- src/CSV.hs
+++ src/CSV.hs
@@ -9,53 +9,73 @@ module CSV  (
             , testQuote
             ) where
 
-import           Data.Attoparsec.Combinator
-import           Data.Attoparsec.Text
-import           Data.Functor
-import           Data.Maybe
-import           Data.Text
-import           Data.Time
+import           Data.Attoparsec.ByteString.Char8 (isAlpha_ascii)
+import           Data.Attoparsec.Combinator       (endOfInput, many1)
+import           Data.Attoparsec.Text             (Parser, char, double,
+                                                   endOfLine, parseOnly,
+                                                   takeTill)
+import qualified Data.Attoparsec.Text             as DAT (takeWhile)
+import qualified Data.Functor                     as DF (($>))
+import           Data.Maybe                       (fromMaybe)
+import           Data.Text                        (Text, unpack)
+import           Data.Time                        (LocalTime (..),
+                                                   TimeOfDay (..),
+                                                   defaultTimeLocale,
+                                                   fromGregorian,
+                                                   iso8601DateFormat,
+                                                   parseTimeM)
 
+-- stock quotes
 data Quote = Quote  {
                       qDate   :: LocalTime,
+                      qStock  :: Text,
                       qShares :: Double,
                       qBasis  :: Double,
                       qPrice  :: Double
                     } deriving (Show, Eq)
 
+-- read stock quotes from CSV file
 csvFile :: Parser [Quote]
 csvFile = many1 quote <* endOfInput
 
+-- parser stock quotes
 quote :: Parser Quote
 quote = Quote <$> (qdate  <* qsep)
+              <*> (qcode  <* qsep)
               <*> (double <* qsep)
               <*> (double <* qsep)
               <*> (double <* endOfLine)
 
--- define the separator
+-- CSV field separator
 qsep :: Parser ()
-qsep = char ',' Data.Functor.$> ()
+qsep = char ',' DF.$> ()
 
--- parse and ISO Date (YYYY-MM-DD)
+-- parse code
+qcode :: Parser Text
+qcode = DAT.takeWhile isAlpha_ascii
+
+-- parse ISO Date (YYYY-MM-DD)
 qdate  :: Parser LocalTime
 qdate  = createDate <$> takeTill (== ',')
          where defaultDate = LocalTime (fromGregorian 0001 01 01) (TimeOfDay 00 00 00 )
                parseDateText t = parseTimeM True Data.Time.defaultTimeLocale (iso8601DateFormat Nothing) (unpack t)
                createDate x = fromMaybe defaultDate $ parseDateText x
 
+-- parser for stock quotes
 quoteParser :: Text -> Either String Quote
 quoteParser = parseOnly quote
 
--- for unit testing
+-- unit testing Text input string
 testString :: Text
-testString = "2018-08-05,100,23.4,25.6\n"
+testString = "2018-08-05,ASX,100,23.4,25.6\n"
 
--- for unit testing
+-- unit testing Quote
 testQuote :: Quote
 testQuote = Quote {
-                    qDate = LocalTime (fromGregorian 2018 08 05) (TimeOfDay 00 00 00),
+                    qDate   = LocalTime (fromGregorian 2018 08 05) (TimeOfDay 00 00 00),
+                    qStock  = "ASX",
                     qShares = 100.0,
-                    qBasis = 23.4,
-                    qPrice = 25.6
+                    qBasis  = 23.4,
+                    qPrice  = 25.6
                   }
 
